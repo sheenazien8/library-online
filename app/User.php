e8c9e7849b71d54316fe866af9f32e11a1014c8a
sheenazien8 <sheenazien08@gmail.com>
2018-04-13 10:15:29 +0700

    Seeder for insert user and admin

--- app/User.php
+++ app/User.php
@@ -8,24 +8,24 @@ use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
-    use LaratrustUserTrait;
-    use Notifiable;
+  use LaratrustUserTrait;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+      'name', 'email', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+      'password', 'remember_token',
+  ];
 }
---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use \App\Role;
+use \App\User;
+class UsersSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    //Make role Admin
+    $roleAdmin = Role::create([
+      'name' => 'admin',
+      'display_name' => 'Admin'
+    ]);
+
+    //make role member
+    $roleMember = Role::create([
+      'name' => 'member',
+      'display_name' => 'Member'
+
+    ]);
+
+    //user example with role admin
+    $adminUser = User::create([
+      'name' => 'Admin Library',
+      'email' => 'admin@gmail.com',
+      'password' => bcrypt('8slamp')
+    ]);
+
+    $adminUser->attachRole($roleAdmin);
+
+    //user example with role member
+    $memberUser = User::create([
+      'name' => 'Member Library',
+      'email' => 'member@gmail.com',
+      'password' => bcrypt('8slamp')
+    ]);
+
+    $memberUser->attachRole($roleMember);
+  }
+}
