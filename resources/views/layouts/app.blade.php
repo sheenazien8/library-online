aa2b41023fe3083b1419b2564fc0ac273fee8303
sheenazien8 <sheenazien08@gmail.com>
2018-04-18 14:20:36 +0700

    Clean code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,6 +87,7 @@
     <script src="{{ asset('js/bootstrap.min.js') }}" ></script>
     <script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('js/dataTables.bootstrap4.min.js') }}" ></script>
+    <script src="{{ asset('js/custom.js') }}" ></script>
 
     @yield('scripts')
 </body>
